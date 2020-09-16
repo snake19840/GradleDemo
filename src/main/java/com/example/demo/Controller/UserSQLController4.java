3a316bb5407def29c6c52fbeeed55e1c77ca18c5
yujian19840 <snake19840@aliyun.com>
2020-09-16 11:05:23 +0800

    JPA-SpecificationExecutor接口测试

--- src/main/java/com/example/demo/Controller/UserSQLController4.java
+++ src/main/java/com/example/demo/Controller/UserSQLController4.java
@@ -22,6 +22,8 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/sql4")
@@ -42,21 +44,12 @@ private UserRepositorySpecification userRepositorySpecification;
 
     @RequestMapping("/search1")
     public String testPagingAndSort(MysqlUser user, Paging paging, Model model){
-        //Pageable ;封装了分页的参数,当前页,每页显示的条数,注意:他的当前页是从0开始的
-        //PageRequest(page,size) page:当前页,size:每页显示的条数
-        Sort.Order order=null;
-        if (paging.isDir()){
-            order =new Sort.Order(Sort.Direction.ASC,paging.getSortObj());
-        }else {
-            order=new Sort.Order(Sort.Direction.DESC,paging.getSortObj());
-        }
 
-        PageRequest pageable = PageRequest.of(paging.getPage()-1, paging.getSize(),Sort.by(order));
 /**
  * Specification<MysqlUser>
  *     封装查询条件
  */
-        Specification<MysqlUser> spce=new Specification<MysqlUser>() {
+        Specification<MysqlUser> spec=new Specification<MysqlUser>() {
             //Predicate:封装单个查询条件
 
             /**
@@ -70,25 +63,125 @@ private UserRepositorySpecification userRepositorySpecification;
             public Predicate toPredicate(Root<MysqlUser> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
                 // where name = "俞健"
                 /**
-                 *  参数一: 查询的属性
+                 *  参数一: 查询的条件属性
                  *  参数二: 条件的值
                  */
-                Predicate pre=criteriaBuilder.equal();
-                return null;
+                Predicate pre=criteriaBuilder.equal(root.get(paging.getSortObj()),user.getUsername());
+                return pre;
             }
-        }
+        };
 
         //遍历当前页
-        Iterable<MysqlUser> list=this.userRepositorySpecification.findAll(spce);
+        try {
+            Iterable<MysqlUser> list=this.userRepositorySpecification.findAll(spec);
+            model.addAttribute("list",list);
+        }catch (Exception e){
+            e.printStackTrace();
+            model.addAttribute("success","没有对应值");
+        }
+        return "Thymeleaf/index4";
+                }
 
 
-        model.addAttribute("list",list);
-        //总条数
-        model.addAttribute("listItems",lists.getTotalElements());
-        //总页数
-        model.addAttribute("listPages",lists.getTotalPages());
+
+    @RequestMapping("/search3")
+    public String testPagingAndSorts(MysqlUser user, Paging paging, Model model){
+
+/**
+ * Specification<MysqlUser>
+ *     封装多条件查询条件
+ */
+        Specification<MysqlUser> spec=new Specification<MysqlUser>() {
+            //Predicate:封装单个查询条件
+
+            /**
+             *
+             * @param root 查询对象属性的封装
+             * @param query 封装了查询各部分的信息  select,from,order by
+             * @param criteriaBuilder 查询条件的构造器,定义不同的查询条件
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<MysqlUser> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+                // where name = "俞健" and password="1"
+            /*    List<Predicate> listPre=new ArrayList<>();
+                listPre.add(criteriaBuilder.equal(root.get("username"),user.getUsername()));
+                listPre.add(criteriaBuilder.equal(root.get("password"),user.getPassword()));
+                Predicate[] arr=new  Predicate[listPre.size()];*/
+
+               /* return criteriaBuilder.and(listPre.toArray(arr));*/
+                //where (name = "俞健" or password="1") and id="1"
+//                return criteriaBuilder.and((criteriaBuilder.or(criteriaBuilder.equal(root.get("username"),user.getUsername()),criteriaBuilder.equal(root.get("password"),user.getPassword()))), criteriaBuilder.equal(root.get("id"),user.getId()));
+                return criteriaBuilder.and(criteriaBuilder.equal(root.get("username"),user.getUsername()),criteriaBuilder.equal(root.get("password"),user.getPassword()));
+            }
+        };
+//        Sort.Order order=new Sort.Order(Sort.Direction.DESC,"id");
+        //遍历当前页
+        try {
+
+            Iterable<MysqlUser> list=this.userRepositorySpecification.findAll(spec);
+           if (!list.iterator().hasNext()){
+               model.addAttribute("success","没有对应值");
+           }else {
+               model.addAttribute("list",list);
+           }
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+            model.addAttribute("success","没有对应值");
+        }
 
         return "Thymeleaf/index4";
     }
 
-}
+    @RequestMapping("/search4")
+    public String testPagingAndSortsOrder(MysqlUser user, Paging paging, Model model){
+
+/**
+ * Specification<MysqlUser>
+ *     封装多条件查询条件
+ */
+        Specification<MysqlUser> spec=new Specification<MysqlUser>() {
+            //Predicate:封装单个查询条件
+
+            /**
+             *
+             * @param root 查询对象属性的封装
+             * @param query 封装了查询各部分的信息  select,from,order by
+             * @param criteriaBuilder 查询条件的构造器,定义不同的查询条件
+             * @return
+             */
+            @Override
+            public Predicate toPredicate(Root<MysqlUser> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
+
+                return criteriaBuilder.and(criteriaBuilder.equal(root.get("username"),user.getUsername()),criteriaBuilder.equal(root.get("password"),user.getPassword()));
+            }
+        };
+        Sort.Order order=null;
+        if (paging.isDir()){
+            order=new Sort.Order(Sort.Direction.DESC,"id");
+        }else {
+             order=new Sort.Order(Sort.Direction.ASC,"id");
+        }
+
+        //遍历当前页
+        try {
+
+            Iterable<MysqlUser> list=this.userRepositorySpecification.findAll(spec,Sort.by(order));
+            if (!list.iterator().hasNext()){
+                model.addAttribute("success","没有对应值");
+            }else {
+                model.addAttribute("list",list);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+
+            model.addAttribute("success","没有对应值");
+        }
+
+        return "Thymeleaf/index4";
+    }
+
+        }
